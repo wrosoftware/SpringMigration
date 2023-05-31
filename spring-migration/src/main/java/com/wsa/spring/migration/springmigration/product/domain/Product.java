29fbb907eeb286be16f1a4f8579d6586d7e14693
WiktorRosinski <wiktor.rosinski@incat.com.pl>
2023-05-31 12:18:42 +0200

    migration spring + jpa

--- spring-migration/src/main/java/com/wsa/spring/migration/springmigration/product/domain/Product.java
+++ spring-migration/src/main/java/com/wsa/spring/migration/springmigration/product/domain/Product.java
@@ -1,12 +1,16 @@
 package com.wsa.spring.migration.springmigration.product.domain;
 
+import jakarta.persistence.Column;
 import lombok.Data;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.Type;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.type.SqlTypes;
+
 import java.math.BigDecimal;
 
 @Data
@@ -20,7 +24,7 @@ class Product {
 
     private String name;
 
-    @Type(type = "text")
+    @Column(columnDefinition = "text")
     private String description;
 
     private BigDecimal price;
--- spring-migration/src/main/java/com/wsa/spring/migration/springmigration/product/domain/ProductApiImpl.java
+++ spring-migration/src/main/java/com/wsa/spring/migration/springmigration/product/domain/ProductApiImpl.java
@@ -1,11 +1,11 @@
 package com.wsa.spring.migration.springmigration.product.domain;
 
 import com.wsa.spring.migration.springmigration.product.ProductApi;
+import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Component
