4954cdd796eb331bdf410bbe59282fe56d93ae22
WiktorRosinski <wiktor.rosinski@incat.com.pl>
2023-06-19 12:00:26 +0200

    updagrade Swagger2 to OpenApi3

--- spring-migration/src/main/java/com/wsa/spring/migration/springmigration/_config/swagger/SwaggerConfig.java
+++ spring-migration/src/main/java/com/wsa/spring/migration/springmigration/_config/swagger/SwaggerConfig.java
@@ -5,87 +5,45 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(title = "spring-migration",
-                    description = "Demo project for migration to  Spring Boot 3.0.x",
-                    version = "1.0.0"),
-        security = @SecurityRequirement(name = "JWT", scopes = "global")
-)
-@SecurityScheme(name = "JWT", type = SecuritySchemeType.HTTP,  scheme = "bearer", bearerFormat = "JWT")
+//@OpenAPIDefinition(
+//        info = @Info(title = "spring-migration",
+//                    description = "Demo project for migration to  Spring Boot 3.0.x",
+//                    version = "1.0.0"),
+//        security = @SecurityRequirement(name = "JWT", scopes = "global")
+//)
+//@SecurityScheme(name = "JWT", type = SecuritySchemeType.HTTP,  scheme = "bearer", bearerFormat = "JWT")
 public class SwaggerConfig implements WebMvcConfigurer {
 
-//    @Value("${application.name}")
-//    public final String title;
-//    @Value("${application.version}")
-//    private final String version;
-//    @Value("${application.description}")
-//    private String description;
+    @Value("${application.name}")
+    public String title;
 
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("swagger-ui.html")
-//                .addResourceLocations("classpath:/META-INF/resources/");
-//        registry.addResourceHandler("/webjars/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-//    }
+    @Value("${application.version}")
+    private String version;
+    @Value("${application.description}")
+    private String description;
 
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addRedirectViewController("/swagger-ui.html", "/swagger-ui/index.html");
-//        registry.addRedirectViewController("/swagger-ui", "/swagger-ui/index.html");
-//    }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title(title)
+                        .version(version)
+                        .description(description))
 
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.OAS_30)
-//                .apiInfo(apiInfo())
-//                .directModelSubstitute(LocalDateTime.class, String.class)
-//                .directModelSubstitute(LocalDate.class, String.class)
-//                .directModelSubstitute(LocalTime.class, String.class)
-//                .securitySchemes(asList(apiKey()))
-//                .securityContexts(asList(securityContext()))
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder()
-//                .title(title)
-//                .version(version)
-//                .description(description)
-//                .build();
-//    }
-//
-//    private ApiKey apiKey() {
-//        return new ApiKey("JWT", "Authorization", "header");
-//    }
-//
-//    private SecurityContext securityContext() {
-//        return SecurityContext.builder()
-//                .securityReferences(asList(securityReference()))
-//                .forPaths(PathSelectors.regex("api/*"))
-//                .build();
-//    }
-//
-//    private SecurityReference securityReference() {
-//        return SecurityReference.builder()
-//                .reference("JWT")
-//                .scopes(scopes())
-//                .build();
-//    }
-//
-//    private AuthorizationScope[] scopes() {
-//        return new AuthorizationScope[]{
-//                new AuthorizationScope("global", "global")
-//        };
-//    }
+                .components(new Components()
+                        .addSecuritySchemes("JWT",
+                                new io.swagger.v3.oas.models.security.SecurityScheme()
+                                        .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
 
 }
